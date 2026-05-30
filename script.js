70aef89e37969e54e01e8f0ba523ad8f4ff7fb44
linus <flyrhinosfly@gmail.com>
2026-05-30 08:43:55 -0400

    working templating

---
+++ script.js
@@ -0,0 +1,33 @@
+class FaqItem {
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+  }
+}
+
+const FAQ_ITEMS = [
+  new FaqItem(
+    "What is Frontend Mentor, and how will it help me?",
+    "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  ),
+  new FaqItem(
+    "Is Frontend Mentor free?",
+    "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  ),
+  new FaqItem(
+    "Can I use Frontend Mentor projects in my portfolio?",
+    "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  ),
+  new FaqItem(
+    "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  ),
+];
+
+let template = document.getElementById("faq-dropdown-item").content;
+FAQ_ITEMS.forEach((data) => {
+  const clone = template.cloneNode(true);
+  clone.querySelector(".question").textContent = data.question;
+  clone.querySelector(".answer").textContent = data.answer;
+  document.body.appendChild(clone);
+});
